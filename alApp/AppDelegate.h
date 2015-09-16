16f79fbe1d6939401c47f550ca04cd9f0498c074
aexonicnitin <n.nikam@aexonic.com>
2015-09-16 14:58:52 +0530

    Initial Source Commit

---
+++ alApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  alApp
+//
+//  Created by Nitin Nikam on 16/09/15.
+//  Copyright (c) 2015 Nitin Nikam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ alApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  alApp
+//
+//  Created by Nitin Nikam on 16/09/15.
+//  Copyright (c) 2015 Nitin Nikam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
